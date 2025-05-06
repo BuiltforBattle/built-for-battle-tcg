f401f1cc3a4bca4b55304f3c56b5a497bfd306be
BuiltforBattle <creativecosplaysmag@gmail.com>
2025-05-06 12:24:41 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -1,12 +1,1 @@
 
-function showMessage(text) {
-  const messageDiv = document.getElementById('gameMessage');
-  if (messageDiv) {
-    messageDiv.innerText = text;
-    setTimeout(() => {
-      messageDiv.innerText = '';
-    }, 4000);
-  } else {
-    alert(text); // Fallback if gameMessage div is not present yet
-  }
-}
