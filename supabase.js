7d76dd01a905264ad7bbc355d8211b426cab6537
BuiltforBattle <creativecosplaysmag@gmail.com>
2025-05-06 12:24:21 -0400

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,1 @@
-const supabase = supabase.createClient(
-    'https://dlmivckwqxkkbsysnfzy.supabase.co', // 👈 paste your Project URL here
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRsbWl2Y2t3cXhra2JzeXNuZnp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY0NDc0NDUsImV4cCI6MjA2MjAyMzQ0NX0.DOeGI6BlgAoHQYlD3anHYCTH_TG4_snHegKDW5LvMTw' // 👈 paste your public API key here
-  );
+
